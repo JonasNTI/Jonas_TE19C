f5c90cf2c65aeb7bf92bd8fdbf6abb42d1e282d2
JonasNTI <jonas.olanders@elev.ga.ntig.se>
2021-05-30 19:29:17 +0200

    4 grafer + kommentarer

--- Programmering i vardagen/Dataanalysprojekt/analys.py
+++ Programmering i vardagen/Dataanalysprojekt/analys.py
@@ -17,36 +17,37 @@ case_tot = df_c.sum()
 icu_tot = df_i.sum()
 death_tot = df_d.sum()
 
-# smittade per åldersgrupp
+# skapar en dataframe för smittade per åldersgrupp
 frekvens_dict_c = dict(age = df_age, smittade = df_c)
 df_age_c = pd.DataFrame(frekvens_dict_c)
 
-# döda per åldersgrupp
+# skapar en dataframe för döda per åldersgrupp
 frekvens_dict_d = dict(age = df_age, dödsfall = df_d)
 df_age_d = pd.DataFrame(frekvens_dict_d)
 
-# totala smittade/döda/icu admissions
+# skapar dataframe för totala antalet smittade/döda/icu admissions
 frekvens_dict_cid = dict(smittade_döda_ICU = ["Smittade", "Döda", "ICU"], antal = [case_tot, death_tot, icu_tot])
 df_cid = pd.DataFrame(frekvens_dict_cid)
-print(df_cid)
 
-# smittade/döda för cirkeldiagram
+# samlar värden för smittade/döda till cirkeldiagram
 döda_smittade = death_tot
 levande_smittade = case_tot - döda_smittade
 
-# skapa fig
+# skapar en allmän figur
 fig = px.bar(df_age_c, x="age", y="smittade", title="Smittade per åldersgrupp")
 
 # utseendet
 app.layout = HTML.Div(children=[
     HTML.H1(children = "Covid-19 statistik"), 
 
+    # dropdownmeny
     dcc.Dropdown(
         id = "drop",
         options = [dict(label = "Cases per åldersgrupp", value="smittade"), dict(label = "Dödsfall per åldersgrupp", value="dödsfall"), dict(label = "smittade_döda_ICU", value="cid"), dict(label = "Smittade/Döda_Procent", value="cd")],
         value="smittade"
     ),
 
+    # rendera visualisering för graferna
     dcc.Graph(
         id = "graph",
         figure = fig
@@ -57,21 +58,24 @@ app.layout = HTML.Div(children=[
     Output("graph", "figure"),
     [Input("drop", "value")]
 )
+
+# funktion som skapar graferna efter vilken knapp i dropdownmenyn som användaren väljer
 def update_figure(value):
     if value == "smittade":
          df = df_age_c
-         fig = px.bar(df, x="age", y=value, title=f"Antal {value} per åldersgrupp")
-    elif value == "dödsfall":
+         fig = px.bar(df, x="age", y=value, title=f"Antal {value} per åldersgrupp") # stapeldiagram (smittade per åldersgrupp)
+    if value == "dödsfall":
         df = df_age_d
-        fig = px.bar(df, x="age", y=value, title=f"Antal {value} per åldersgrupp")
-    elif value == "cid":
+        fig = px.bar(df, x="age", y=value, title=f"Antal {value} per åldersgrupp") # stapeldiagram (döda per åldersgrupp)
+    if value == "cid":
         df = df_cid
-        fig = px.bar(df, x="antal", y="smittade_döda_ICU", orientation="h", title=f"Totalt antal smittade, döda och intensivvårdade")
-    elif value == "cd":
-        fig = px.pie(names=["levande som är/varit smittade", "smittade som dött"], values=[levande_smittade,döda_smittade], title=f"Procentuell andel av alla covid fall som lett till död")
+        fig = px.bar(df, x="antal", y="smittade_döda_ICU", orientation="h", title=f"Totalt antal smittade, döda och intensivvårdade") # liggande stapeldiagram (antal smittade/döda/ICU)
+    if value == "cd":
+        fig = px.pie(names=["levande som är/varit smittade", "smittade som dött"], values=[levande_smittade,döda_smittade], title=f"Procentuell andel av alla covid fall som lett till död") # cirkeldiagram (smittade som har/inte har dött)
 
-    fig.update_layout(transition_duration=500)
-    return fig
+    fig.update_layout(transition_duration=500) # tiden det tar för en graf att bytas ut mot en annan
+    return fig  # returnerar figuren(grafen)
 
+# startar server i webbläsaren
 if __name__ == "__main__":
     app.run_server(debug = True)
